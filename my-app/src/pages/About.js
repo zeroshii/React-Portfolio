084a3bacab52e463523b1de39f2afe7c140eaa7f
Garman_K <garnetbirthstone@hotmail.com>
2020-11-24 09:11:10 -0500

    update pages

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -3,8 +3,8 @@ function About() {
         <main className="container">
            
                 <h1>About Me</h1>
-                <hr>
-                <img src="https://cdn.discordapp.com/attachments/229787993630703616/757384971475484722/me.jpg" className="rounded float-left p-4 img-fluid max-width: 100%" alt="Portrait">
+                <hr></hr>
+                <img src="https://cdn.discordapp.com/attachments/229787993630703616/757384971475484722/me.jpg" className="rounded float-left p-4 img-fluid max-width: 100%" alt="Portrait"/>
                 <p>Hello! I am Garman Kwan, a web designer/developer based in Toronto, Canada.</p>
                 <p>I enjoy working with computers and creating beautiful, functional, and intuitive websites.</p>
                 <p> I like to read, write, design enamel pins, and play the piano in my free time.</p>
