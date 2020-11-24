a046b4dc463457761fec85fe4c2eddc50167e8d2
Garman_K <garnetbirthstone@hotmail.com>
2020-11-24 08:59:27 -0500

    with react

---
+++ my-app/src/pages/Contact.js
@@ -0,0 +1,7 @@
+function Contact() {
+    return (
+        <h1>hello</h1>
+    )
+}
+
+export default Contact;
---
+++ my-app/src/pages/Portfolio.js
@@ -0,0 +1,7 @@
+function Portfolio() {
+    return (
+        <h1>hello</h1>
+    )
+}
+
+export default Portfolio;
